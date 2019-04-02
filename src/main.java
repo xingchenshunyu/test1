1426cee91b546b56ff7efb1204c545cb75767565
xingchenshunyu <3157463498@qq.com>
2019-04-02 09:20:27 +0800

    git test

--- src/main.java
+++ src/main.java
@@ -1,2 +1,8 @@
 public class main {
+    public static void main(String[] args) {
+        final char sex1 = '男';
+        final char sex2 = '女';
+        System.out.println(sex1);
+        System.out.println(sex2);
+    }
 }
